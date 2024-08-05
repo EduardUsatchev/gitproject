97b59985abcd08cc6d4d1b32a991ff99bc41a692
Eduard Usatchev <eduard.usatchev@gmail.com>
2024-08-05 20:38:49 +0300

    Adding message function

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def disk_resize(disk_name, machine_name,target_gb):
     print("locating machine")
     print("resizing disk")
     print("SSHing to machine")
+def message():
+    print("The resizing procedure is end")
 
 disk_name = getenv("DISK_NAME")
 machine_name = getenv("MACHINE_NAME")
 target_gb = getenv("TARGET_GB")
 
 disk_resize(disk_name,machine_name,target_gb)
+if True:
+    message()
 print("End of the script")
